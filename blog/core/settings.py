e39807fca15fe95f2f33ee1374df77ab142e217f
Artem Jasan <artem.jasan@applifting.cz>
2023-05-22 18:53:46 +0200

    Add checkers, pre-commit, cicd

--- blog/core/settings.py
+++ blog/core/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +22,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
-    "blog_app",
 ]
 
 MIDDLEWARE = [
